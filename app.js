4b5764f32fcf0755fde495f33087cb95660470ad
Bell <dante.be@hotmail.com>
2025-07-10 14:26:56 -0300

    modificaciones app + dependencias nuevas

---
+++ app.js
@@ -0,0 +1,1 @@
+import express from "express"; const app = express(); const port = 5000; app.get("/", (request, response) => { response.send("Hello World!"); }); app.listen(5000, () => { console.log(`Server is running on port ${port}`); }); 
