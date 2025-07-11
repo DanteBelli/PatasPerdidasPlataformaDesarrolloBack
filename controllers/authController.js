64ce81f45c03d80fe0a19866ae8788299c95895e
Bell <dante.be@hotmail.com>
2025-07-11 16:01:16 -0300

    Se agrega los controladores para crear un usuario y el log in de este mismo

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,27 +1,40 @@
-const { error } = require("console");
-const db = require("../config/db");
-const bcrypt = require("bcrypt");
+import { error } from "console";
+import db from "../config/db.js";
+import bcrypt from "bcryptjs";
 
-exports.register = async (req , res) => {
-    const {mail, password} = req.body;
-
-    const [existe] =await db.query("Select * from usuarios where email = ?",[mail]);
-    if (existe.length > 0)return res.status(400).json({error:"Usuario ya existente"});
-
-    const hashed = await bcrypt.hash(password,10);
-    await db.query("Insert into usuarios(mail,password,rol)VALUES(?,?,?)",[mail,hashed,"usuario"]);
-
-    res.status(201).json({message:"Usuario Creado"});
+export const register = async (req ,res)=>{
+    const {mail , password,rol} = req.body;
+    try{
+        const hashedPass = await bcrypt.hash(password,10);
+         const [exist] = await db.query("SELECT * FROM usuarios where mail = ?",[mail]);
+         if(exist.length>0){
+            return res.status(400).json({error:"Error"});
+         }
+         await db.query("Insert into usuarios (mail, password,rol)VALUES(?,?,?)"),[mail,password,rol];
+         res.status(201).json({message : "Usuario creado"});
+    }catch(error){
+        console.error("Error",error);
+        res.status(500).json({error:"error"});
+    }
 };
 
-exports.login = async (req , res) =>{
-    const {mail , password} = req.body;
-    const [usuarios] = await db.query("SELECT * FROM usuarios where mail = ?",[mail]);
-    if(usuarios.length ===0)return res.status(404).json({error : "No existe este usuario"});
+export const login = async (req , res)=>{
+    const{email,password}=req.body;
 
-    const ok = await bcrypt.compare(password , usuarios[0].password);
-    if(!ok)return res.status(401).json({error: "Error en la constraseña"});
+    try{
+        const [usuarios]=await db.query("select * from usuarios where mail = ?",[email]);
+        const usuario = usuarios[0];
 
-    res.json({id: usuarios[0].id, mail: usuarios[0].mail });
+        if(!usuario){
+            return res.status(404).json({error:"No se encuentra el user"});
+        }
+        const passwordOk = await bcrypt.compare(password,usuario.password);
+        if (!passwordOk){
+            return res.status(401).json({error:"No se encuentra el usuario"});
+        }
+        res.json({message:"Login Correcto" , usuario: {id: usuario.id , email: usuario.mail}});
+    }catch (error){
+        console.error("Error en el log",error);
+        res.status(500).json({error: "Error"});
+    }
 };
-
