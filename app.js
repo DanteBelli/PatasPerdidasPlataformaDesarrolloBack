4677bc77d1fb13ddfd329668a28457d9700059b0
Bell <dante.be@hotmail.com>
2025-07-11 12:28:01 -0300

    Modificaciones en el script para un correcto funcuinamiento

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-import express from "express"; const app = express(); const port = 5000; app.get("/", (request, response) => { response.send("Hello World!"); }); app.listen(5000, () => { console.log(`Server is running on port ${port}`); }); 
+import express from "express";
+const app = express();
+const port = 5000;
+app.get("/", (request, response) =>
+    {
+        response.send("Prueba PatasPerdidas");
+    });
+app.listen(5000, () =>
+    {
+        console.log(`Puerto NRO ${port}`);
+    }); 
--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "prueba123",
+    password: "",
     database: "patasperdidas",
     waitForConnections: true,
     connectionLimit: 10,
