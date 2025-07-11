1f36a277e2e735650de60fd8fd5d54c4791508ac
Bell <dante.be@hotmail.com>
2025-07-11 15:28:19 -0300

    Modificacion controlador nombre de campos

--- controllers/mascotaController.js
+++ controllers/mascotaController.js
@@ -12,7 +12,7 @@ export const crearMascota = async (req, res) => {
   try {
     const { tipo, descripcion, usuario_email, foto, nombre, lat, lng } = req.body;
     const [resul] = await db.query(
-      "INSERT INTO mascotas (tipo, descripcion, usuario_email, lat, lng, foto, nombre) VALUES (?, ?, ?, ?, ?, ?, ?)",
+      "INSERT INTO mascotas (tipo, descripcion, usuariMail, lat, lng, foto, nombre) VALUES (?, ?, ?, ?, ?, ?, ?)",
       [tipo, descripcion, usuario_email, lat, lng, foto, nombre]
     );
     res.status(201).json({ message: "Creación exitosa", id: resul.insertId });
