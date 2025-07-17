4bc0e9991d6feb0a41571d3d82f972daa0c8f9ca
DanteBell95 <dante.belli18@gmail.com>
2025-07-17 11:12:36 -0300

    Se modifica en auth , la ruta de crear un usuario nuevo , por error de tipeo

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { register,login } from "../controllers/authController.js";
 const router = Router();
-router.post("/registar",register);
+router.post("/register",register);
 router.post("/login",login);
 export default router;
